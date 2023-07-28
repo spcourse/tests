e8ad75b4ec54eec88b13169e20e06dd2852c62f5
Jelleas <jelle.van.assema@gmail.com>
2023-07-28 18:27:59 +0200

    indent with spaces

--- tests/movement/appleTest.py
+++ tests/movement/appleTest.py
@@ -8,44 +8,44 @@ monkeypatch.patchMatplotlib()
 monkeypatch.patchNumpy()
 
 def restrict(state: declarative.FunctionState):
-	assert ast.Break not in static.AbstractSyntaxTree(state.fileName)
+    assert ast.Break not in static.AbstractSyntaxTree(state.fileName)
 
 def apple1Hint(state: declarative.FunctionState):
-	t, v = state.returned
-	if v == approx(159.47, abs=1) or t == approx(4.52, abs=0.1):
-		raise AssertionError("Did you mix up the order of the return values?")
+    t, v = state.returned
+    if v == approx(159.47, abs=1) or t == approx(4.52, abs=0.1):
+        raise AssertionError("Did you mix up the order of the return values?")
 
-	if v == approx(44.3, abs=0.3):
-		raise AssertionError("Did you forget to convert to km/h?")
+    if v == approx(44.3, abs=0.3):
+        raise AssertionError("Did you forget to convert to km/h?")
 
 simulate_apple1 = (
-	declarative.function("simulate_apple1")
-	.do(restrict)
-	.params("x", "dt")
+    declarative.function("simulate_apple1")
+    .do(restrict)
+    .params("x", "dt")
     .returnType(Tuple[float, float])
 )
 
 testAppleDef1 = test()(simulate_apple1)
 
 testApple1 = passed(testAppleDef1, hide=False)(
-	simulate_apple1
-	.call(100, 0.01)
-	.do(apple1Hint)
-	.returns((approx(159.47, abs=0.1), approx(4.52, abs=0.1)))
+    simulate_apple1
+    .call(100, 0.01)
+    .do(apple1Hint)
+    .returns((approx(159.47, abs=0.1), approx(4.52, abs=0.1)))
 )
 
 
 simulate_apple2 = (
-	declarative.function("simulate_apple2")
-	.do(restrict)
-	.params("x")
-	.returnType(float)
+    declarative.function("simulate_apple2")
+    .do(restrict)
+    .params("x")
+    .returnType(float)
 )
 
 testAppleDef2 = test()(simulate_apple2)
 
 testApple2 = passed(testAppleDef2, hide=False)(
-	simulate_apple2
-	.call(0.01)
-	.returns(approx(2.84, abs=0.02))
+    simulate_apple2
+    .call(0.01)
+    .returns(approx(2.84, abs=0.02))
 )
--- tests/movement/basejumpTest.py
+++ tests/movement/basejumpTest.py
@@ -9,62 +9,62 @@ monkeypatch.patchNumpy()
 
 
 def validate_sim(input_params, expected_time, xl, tl):
-	start_height = input_params[0]
-	end_height = input_params[1]
+    start_height = input_params[0]
+    end_height = input_params[1]
 
-	assert len(xl) == len(tl), "function is expected to return two lists of the same length"
+    assert len(xl) == len(tl), "function is expected to return two lists of the same length"
 
-	x_start, x_end = xl[0], xl[-1]
-	t_start, t_end = tl[0], tl[-1]
+    x_start, x_end = xl[0], xl[-1]
+    t_start, t_end = tl[0], tl[-1]
 
-	if t_end == approx(expected_time, abs=0.2):
-		return
+    if t_end == approx(expected_time, abs=0.2):
+        return
 
-	assert x_start != approx(start_height, abs=0.1),\
-		"It looks like the simulation did not start at a height of 1000 meters."
-	
-	assert x_end != approx(end_height, abs=1),\
-		"It looks like the simulation did not stop at a height of 200 meters."
-	
-	assert t_start != approx(0, abs=0.1),\
-		"It looks like the starting time of the simulation is not 0."
+    assert x_start != approx(start_height, abs=0.1),\
+        "It looks like the simulation did not start at a height of 1000 meters."
+    
+    assert x_end != approx(end_height, abs=1),\
+        "It looks like the simulation did not stop at a height of 200 meters."
+    
+    assert t_start != approx(0, abs=0.1),\
+        "It looks like the starting time of the simulation is not 0."
 
-	raise AssertionError(f"Did not expect a total elapsed time of {tl[-1]} (with input {input_params})")
+    raise AssertionError(f"Did not expect a total elapsed time of {tl[-1]} (with input {input_params})")
 
 
 def restrict(state: declarative.FunctionState):
-	assert ast.Break not in static.AbstractSyntaxTree()
+    assert ast.Break not in static.AbstractSyntaxTree()
 
 simulate_free_fall = (
-	declarative.function("simulate_free_fall")
-	.do(restrict)
-	.params("x_start", "x_end", "m", "xi", "dt")
-	.returnType(typing.Tuple[typing.List[float], typing.List[float]])
+    declarative.function("simulate_free_fall")
+    .do(restrict)
+    .params("x_start", "x_end", "m", "xi", "dt")
+    .returnType(typing.Tuple[typing.List[float], typing.List[float]])
 )
 
 testDef = test()(simulate_free_fall)
 
 testBasejump1 = passed(testDef, timeout=90, hide=False)(
-	simulate_free_fall
-	.call(1000.0, 200.0, 72.0, 0, 0.01)
-	.do(lambda state: validate_sim(state.args, 12.77, state.returned[0], state.returned[1]))
-	.description("Test simulation without air resistance.")
+    simulate_free_fall
+    .call(1000.0, 200.0, 72.0, 0, 0.01)
+    .do(lambda state: validate_sim(state.args, 12.77, state.returned[0], state.returned[1]))
+    .description("Test simulation without air resistance.")
 )
 
 testBasejump2 = passed(testDef, timeout=90, hide=False)(
-	simulate_free_fall
-	.call(1000.0, 200.0, 72.0, 0.24, 0.01)
-	.do(lambda state: validate_sim(state.args, 18.58, state.returned[0], state.returned[1]))
-	.description("Test simulation with air resistance.")
+    simulate_free_fall
+    .call(1000.0, 200.0, 72.0, 0.24, 0.01)
+    .do(lambda state: validate_sim(state.args, 18.58, state.returned[0], state.returned[1]))
+    .description("Test simulation with air resistance.")
 )
 
 testBasejump3 = passed(testDef, timeout=90, hide=False)(
-	simulate_free_fall
-	.call(1200, 300, 78, 0.24, 0.01)
-	.do(lambda state: validate_sim(state.args, 19.93, state.returned[0], state.returned[1]))
+    simulate_free_fall
+    .call(1200, 300, 78, 0.24, 0.01)
+    .do(lambda state: validate_sim(state.args, 19.93, state.returned[0], state.returned[1]))
 )
 
 @test()
 def showsGraph():
-	"""Either saves a graph into a file, or shows a graph on the screen."""
-	assert "savefig" in static.getFunctionCalls() or "show" in static.getFunctionCalls()
+    """Either saves a graph into a file, or shows a graph on the screen."""
+    assert "savefig" in static.getFunctionCalls() or "show" in static.getFunctionCalls()
