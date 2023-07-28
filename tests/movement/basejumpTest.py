5aa690998597c06228a3eb4c7a86d364dd562af2
Jelleas <jelle.van.assema@gmail.com>
2023-07-28 16:12:15 +0200

    rewrite movement/basejump to builder style

--- tests/movement/basejumpTest.py
+++ tests/movement/basejumpTest.py
@@ -1,147 +1,77 @@
-import checkpy.tests as t
-import checkpy.lib as lib
-import checkpy.assertlib as assertlib
-import importlib
+import ast
+import typing
 
-import os
-import sys
+from checkpy import *
 
-parpath = os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir, os.pardir))
-sys.path.append(parpath)
-
-from notAllowedCode import *
-from helpers import apply_function, InvalidFunctionApplication, similar, has_function
-
-def before():
-	try:
-		import matplotlib
-		import warnings
-		warnings.filterwarnings("ignore")
-		matplotlib.use("Agg")
-		import matplotlib.pyplot as plt
-		plt.switch_backend("Agg")
-		lib.neutralizeFunction(plt.pause)
-		lib.neutralizeFunction(plt.show)
-	except ImportError:
-		pass
-
-	try:
-		import numpy
-		numpy.seterr('raise')
-	except ImportError:
-		pass
-
-
-def after():
-	try:
-		import matplotlib.pyplot as plt
-		plt.switch_backend("TkAgg")
-		importlib.reload(plt)
-	except ImportError:
-		pass
-
-
-@t.test(0)
-def containsRequiredFunctionDefinitions(test):
-
-	notAllowed = {"break": "break"}
-	notAllowedCode(test, lib.source(_fileName), notAllowed)
-	has_function(test, _fileName, "simulate_free_fall", ['x_start', 'x_end', 'm', 'xi', 'dt'])
-
+only("basejump.py")
+monkeypatch.patchMatplotlib()
+monkeypatch.patchNumpy()
 
 
 def validate_sim(input_params, expected_time, xl, tl):
 	start_height = input_params[0]
 	end_height = input_params[1]
-	_x_start, _x_end = xl[0], xl[-1]
-	_t_start, _t_end = tl[0], tl[-1]
 
-	if not all([type(v) == float or type(v) == int for v in [_x_start, _x_end, _t_start, _t_end]]):
-		return False, "Both lists should only contain floats."
+	assert len(xl) == len(tl), "function is expected to return two lists of the same length"
 
-	if similar(_t_end, expected_time, 0.2):
-		return True, ""
+	x_start, x_end = xl[0], xl[-1]
+	t_start, t_end = tl[0], tl[-1]
 
-	if not similar(_x_start, start_height, 0.1):
-		return False, "It looks like the simulation did not start at a height of 1000 meters."
-	if not similar(_x_end, end_height, 1):
-		return False, "It looks like the simulation did not stop at a height of 200 meters."
-	if not similar(_t_start, 0, 0.1):
-		return False, "It looks like the starting time of the simulation is not 0."
+	if t_end == approx(expected_time, abs=0.2):
+		return
 
-	return False, f"Did not expect a total elapsed time of {tl[-1]} (with input {input_params})"
+	assert x_start != approx(start_height, abs=0.1),\
+		"It looks like the simulation did not start at a height of 1000 meters."
+	
+	assert x_end != approx(end_height, abs=1),\
+		"It looks like the simulation did not stop at a height of 200 meters."
+	
+	assert t_start != approx(0, abs=0.1),\
+		"It looks like the starting time of the simulation is not 0."
 
-
-@t.passed(containsRequiredFunctionDefinitions)
-@t.test(1)
-def testBasejump1(test):
-	def testMethod():
-		try:
-			_input = 1000.0, 200.0, 72.0, 0, 0.01
-			_fn = lib.getFunction("simulate_free_fall", _fileName)
-			_xl, _tl = apply_function(_fn, _input, (list, list))
-		except InvalidFunctionApplication as e:
-			return False, e.message
-		except Exception as e:
-			return False, f"An error occured while running the function: \n {type(e).__name__}: {str(e)}"
-
-		if len(_xl) != len(_tl):
-			return False, "Function is expected to return two lists of the same length."
-
-		return validate_sim(_input, 12.77, _xl, _tl)
+	raise AssertionError(f"Did not expect a total elapsed time of {tl[-1]} (with input {input_params})")
 
 
-	test.test = testMethod
-	test.description = lambda : "Test simulation without air resistance. Starting at 1000m and ending at 200m."
-	test.timeout = lambda : 90
+def restrict(state: builder.FunctionState):
+	assert ast.Break not in static.AbstractSyntaxTree()
 
-@t.passed(containsRequiredFunctionDefinitions)
-@t.test(2)
-def testBasejump2(test):
-	def testMethod():
-		try:
-			_input = 1000.0, 200.0, 72.0, 0.24, 0.01
-			_fn = lib.getFunction("simulate_free_fall", _fileName)
-			_xl, _tl = apply_function(_fn, _input, (list, list))
-		except InvalidFunctionApplication as e:
-			return False, e.message
-		except Exception as e:
-			return False, f"An error occured while running the function: \n {type(e).__name__}: {str(e)}"
+testDef = (builder
+	.function("simulate_free_fall")
+	.do(restrict)
+	.params("x_start", "x_end", "m", "xi", "dt")
+	.build()
+)
 
-		if len(_xl) != len(_tl):
-			return False, "Function is expected to return two lists of the same length."
+testBasejumpNew1 = passed(testDef, hide=False)(builder
+	.function("simulate_free_fall")
+	.timeout(90)
+	.returnType(typing.Tuple[typing.List[float], typing.List[float]])
+	.call(1000.0, 200.0, 72.0, 0, 0.01)
+	.do(lambda state: validate_sim(state.args, 12.77, state.returned[0], state.returned[1]))
+	.description("Test simulation without air resistance.")
+	.build()
+)
 
-		return validate_sim(_input, 18.58, _xl, _tl)
+testBasejumpNew2 = passed(testDef, hide=False)(builder
+	.function("simulate_free_fall")
+	.timeout(90)
+	.returnType(typing.Tuple[typing.List[float], typing.List[float]])
+	.call(1000.0, 200.0, 72.0, 0.24, 0.01)
+	.do(lambda state: validate_sim(state.args, 18.58, state.returned[0], state.returned[1]))
+	.description("Test simulation with air resistance.")
+	.build()
+)
 
+testBasejumpNew3 = passed(testDef, hide=False)(builder
+	.function("simulate_free_fall")
+	.timeout(90)
+	.returnType(typing.Tuple[typing.List[float], typing.List[float]])
+	.call(1200, 300, 78, 0.24, 0.01)
+	.do(lambda state: validate_sim(state.args, 19.93, state.returned[0], state.returned[1]))
+	.build()
+)
 
-	test.test = testMethod
-	test.description = lambda : "Test simulation with air resistance. Starting at 1000m and ending at 200m."
-	test.timeout = lambda : 90
-
-@t.passed(containsRequiredFunctionDefinitions)
-@t.test(3)
-def testBasejump3(test):
-	def testMethod():
-		try:
-			_input = 1200, 300, 78, 0.24, 0.01
-			_fn = lib.getFunction("simulate_free_fall", _fileName)
-			_xl, _tl = apply_function(_fn, _input, (list, list))
-		except InvalidFunctionApplication as e:
-			return False, e.message
-		except Exception as e:
-			return False, f"An error occured while running the function: \n {type(e).__name__}: {str(e)}"
-
-		if len(_xl) != len(_tl):
-			return False, "Function is expected to return two lists of the same length."
-
-		return validate_sim(_input, 19.93, _xl, _tl)
-
-
-	test.test = testMethod
-	test.description = lambda : f"Test simulation with paramaters 1200, 300, 78, 0.24, 0.01."
-	test.timeout = lambda : 90
-
-@t.test(4)
-def showsGraph(test):
-	test.test = lambda : assertlib.fileContainsFunctionCalls(_fileName, "savefig") or assertlib.fileContainsFunctionCalls(_fileName, "show")
-	test.description = lambda : "Either saves a graph into a file, or shows a graph on the screen."
+@test()
+def showsGraph():
+	"""Either saves a graph into a file, or shows a graph on the screen."""
+	assert "savefig" in static.getFunctionCalls() or "show" in static.getFunctionCalls()
