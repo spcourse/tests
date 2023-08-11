67d3b01e014fc3745267caadc32775dc61b19445
Jelleas <jelle.van.assema@gmail.com>
2023-08-11 15:15:22 +0200

    numbers/prime rewrite

--- tests/numbers/primeTest.py
+++ tests/numbers/primeTest.py
@@ -1,32 +1,26 @@
-import checkpy.tests as t
 import checkpy.lib as lib
 import checkpy.assertlib as assertlib
 
-import os
-import sys
+import ast
+from checkpy import *
 
-parpath = os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir, os.pardir))
-sys.path.append(parpath)
 
-from notAllowedCode import *
+@test()
+def exact1():
+	"""input of 1 yields output of 2"""
+	assert ast.Break not in static.AbstractSyntaxTree()
+	assert ast.List not in static.AbstractSyntaxTree()
 
-@t.test(0)
-def exact1(test):
+	assert 2 in static.getNumbersFrom(outputOf(stdinArgs=[1]))
 
-	notAllowed = {"lists": "[", "break": "break"}
-	notAllowedCode(test, lib.source(_fileName), notAllowed)
 
-	test.test = lambda : assertlib.numberOnLine(2, lib.getLine(lib.outputOf(_fileName, stdinArgs=[1]), 0))
-	test.description = lambda : "input of 1 yields output of 2"
+@passed(exact1, timeout=90, hide=False)
+def exact1000():
+	"""input of 1000 yields output of 7919"""
+	assert 7919 in static.getNumbersFrom(outputOf(stdinArgs=[1000]))
 
-@t.test(10)
-def exact1000(test):
-	test.test = lambda : assertlib.numberOnLine(7919, lib.getLine(lib.outputOf(_fileName, stdinArgs=[1000]), 0))
-	test.description = lambda : "input of 1000 yields output of 7919"
-	test.timeout = lambda : 90
 
-@t.passed(exact1)
-@t.test(30)
-def handlesWrongInput(test):
-	test.test = lambda : assertlib.numberOnLine(2, lib.getLine(lib.outputOf(_fileName, stdinArgs=[-90, -1, 0, 1]), 0))
-	test.description = lambda : "rejects negative input and rejects input of zero"
+@passed(exact1, timeout=90, hide=False)
+def handlesWrongInput():
+	"""rejects negative input and rejects input of zero"""
+	assert 2 in static.getNumbersFrom(outputOf(stdinArgs=[-90, -1, 0, 1]))
