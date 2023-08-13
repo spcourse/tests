676cf5f0d2b0fbc82c7580a76b4359af273b0ca7
Jelleas <jelle.van.assema@gmail.com>
2023-08-13 12:23:03 +0200

    text/sentiment rewrite

--- tests/text/sentimentTest.py
+++ tests/text/sentimentTest.py
@@ -1,87 +1,32 @@
-import checkpy.tests as t
-import checkpy.lib as lib
-import checkpy.assertlib as assertlib
-import importlib
-
 # Test once again test if the function is in the code with the right amount of
 # arguments and right type of return, but only gives a mesage at test 0.
 
-def sandbox():
-	lib.require("pos_words.txt", "https://github.com/spcourse/text/raw/main/en/sentiment/pos_words.txt")
-	lib.require("neg_words.txt", "https://github.com/spcourse/text/raw/main/en/sentiment/neg_words.txt")
+from checkpy import *
 
+only("sentiment.py")
+download("pos_words.txt", "https://github.com/spcourse/text/raw/main/en/sentiment/pos_words.txt")
+download("neg_words.txt", "https://github.com/spcourse/text/raw/main/en/sentiment/neg_words.txt")
 
+sentiment_of_text = (declarative
+    .function("sentiment_of_text")
+    .params("text")
+    .returnType(int)
+)
 
-@t.test(0)
-def Correctfor_pos(test):
-    test_text = "Coronet has the best lines of all day cruisers."
-    function_name = "sentiment_of_text"
+correctForPos = test()(sentiment_of_text
+    .call("Coronet has the best lines of all day cruisers.")
+    .returns(1)
+    .description("recognises a positive sentence")
+)
 
-    def testMethod():
-        hasfunction = assertlib.fileContainsFunctionDefinitions(_fileName, function_name)
-        if not hasfunction:
-            info = "Make sure you use a function named " + function_name + " as you're writing your code."
-            return False, info
+correctForNeutral = test()(sentiment_of_text
+    .call("Bertram has a deep V hull and runs easily through seas.")
+    .returns(0)
+    .description("recognises a neutral sentence")
+)
 
-        nArguments = len(lib.getFunction(function_name, _fileName).arguments)
-        if nArguments != 1:
-            info = "Make sure your function has 1 argument."
-            return False, info
-
-        correct_return = assertlib.sameType(lib.getFunction(function_name, _fileName)(test_text), 1)
-        if not correct_return:
-            info = "Make sure you return an list and nothing else."
-            return False, info
-
-        return assertlib.exact(lib.getFunction(function_name, _fileName)(test_text), 1)
-
-    test.test = testMethod
-    test.description = lambda : "Recognises a positive sentence."
-
-
-@t.test(1)
-def Correctfor_neu(test):
-    test_text = "Bertram has a deep V hull and runs easily through seas."
-    function_name = "sentiment_of_text"
-
-    def testMethod():
-        hasfunction = assertlib.fileContainsFunctionDefinitions(_fileName, function_name)
-        if not hasfunction:
-            return False
-
-        nArguments = len(lib.getFunction(function_name, _fileName).arguments)
-        if nArguments != 1:
-            return False
-
-        correct_return = assertlib.sameType(lib.getFunction(function_name, _fileName)(test_text), 1)
-        if not correct_return:
-            return False
-
-        return assertlib.exact(lib.getFunction(function_name, _fileName)(test_text), 0)
-
-    test.test = testMethod
-    test.description = lambda : "Recognises a neutral sentence."
-
-
-@t.test(3)
-def Correctfor_neg(test):
-    test_text = "I dislike old cabin cruisers."
-    function_name = "sentiment_of_text"
-
-    def testMethod():
-        hasfunction = assertlib.fileContainsFunctionDefinitions(_fileName, function_name)
-        if not hasfunction:
-            return False
-
-        nArguments = len(lib.getFunction(function_name, _fileName).arguments)
-        if nArguments != 1:
-            return False
-
-        correct_return = assertlib.sameType(lib.getFunction(function_name, _fileName)(test_text), 1)
-        if not correct_return:
-            return False
-
-        return assertlib.exact(lib.getFunction(function_name, _fileName)(test_text), -1)
-
-    test.test = testMethod
-    test.description = lambda : "Recognises a negative sentence."
+correctForNeg = test()(sentiment_of_text
+    .call("I dislike old cabin cruisers.")
+    .returns(-1)
+    .description("recognises a negative sentence")
+)
