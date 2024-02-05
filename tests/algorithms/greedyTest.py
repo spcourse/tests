fd60079e5706f38ce3c6e9ee54dc821513161194
Roan <roanvanblanken.rvb@gmail.com>
2024-02-05 17:47:56 +0100

    Updated test for water.py and greedy.py, so they also accept output with text

--- tests/algorithms/greedyTest.py
+++ tests/algorithms/greedyTest.py
@@ -12,35 +12,35 @@ def noBreakAndImport():
 
 
 @passed(noBreakAndImport, hide=False)
-def handlesWrongInput():
-	"""rejects negative input, then accepts an input of 0.41"""
-	assert "4\n" == outputOf(stdinArgs=[-1, -1, 0.41])
-
-
-@passed(noBreakAndImport, hide=False)
 def exactChange0():
 	"""input of 0 yields output of 0"""
-	assert "0\n" == outputOf(stdinArgs=[0])
+	assert 0 in static.getNumbersFrom(outputOf(stdinArgs=[0]))
 
 
 @passed(noBreakAndImport, hide=False)
 def exactChange41():
 	"""input of 0.41 yields output of 4"""
-	assert "4\n" == outputOf(stdinArgs=[0.41])
+	assert 4 in static.getNumbersFrom(outputOf(stdinArgs=[0.41]))
 
 @passed(noBreakAndImport, hide=False)
 def exactChange001():
 	"""input of 0.01 yields output of 1"""
-	assert "1\n" == outputOf(stdinArgs=[0.01])
+	assert 1 in static.getNumbersFrom(outputOf(stdinArgs=[0.01]))
 
 
 @passed(noBreakAndImport, hide=False)
 def exactChange16():
 	"""input of 1.6 yields output of 7"""
-	assert "7\n" == outputOf(stdinArgs=[1.6])
+	assert 7 in static.getNumbersFrom(outputOf(stdinArgs=[1.6]))
 
 
 @passed(noBreakAndImport, hide=False)
 def exactChange420():
 	"""input of 4.2 yields output of 18"""
-	assert "18\n" == outputOf(stdinArgs=[4.2]), "did you forget to round your input to the nearest cent?"
+	assert 18 in static.getNumbersFrom(outputOf(stdinArgs=[4.2]))
+
+
+@passed(noBreakAndImport, hide=False)
+def handlesWrongInput():
+	"""rejects negative input, then accepts an input of 0.41"""
+	assert 4 in static.getNumbersFrom(outputOf(stdinArgs=[-1, -1, 0.41]))
--- tests/algorithms/waterTest.py
+++ tests/algorithms/waterTest.py
@@ -5,16 +5,16 @@ only("water.py")
 @test()
 def exactWater0():
 	"""0 minutes in the shower is 0 bottles of water"""
-	assert "0\n" == outputOf(stdinArgs=[0])
+	assert 0 in static.getNumbersFrom(outputOf(stdinArgs=[0]))
 
 
 @test()
 def exactWater10():
 	"""10 minutes in the shower is 120 bottles of water"""
-	assert "120\n" == outputOf(stdinArgs=[10])
+	assert 120 in static.getNumbersFrom(outputOf(stdinArgs=[10]))
 
 
 @test()
 def exactWater327():
 	"""327 minutes in the shower is 3924 bottles of water"""
-	assert "3924\n" == outputOf(stdinArgs=[327])
+	assert 3924 in static.getNumbersFrom(outputOf(stdinArgs=[327]))
