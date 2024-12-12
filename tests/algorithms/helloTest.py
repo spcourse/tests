2c489d5157fc9b71676205a4f0d2122a94d0c9df
simonpauw <simonpauw@gmail.com>
2024-12-12 15:08:15 +0100

    hello

--- tests/algorithms/helloTest.py
+++ tests/algorithms/helloTest.py
@@ -3,11 +3,11 @@ from checkpy import *
 only("hello.py")
 
 @test()
-def exactHello():
-	"""prints \"Hello, world!\""""
-	assert "Hello, world!\n" == outputOf()
-
-@failed(exactHello)
 def oneLine():
 	"""prints exactly 1 line of output"""
 	assert 1 == outputOf().count("\n")
+
+@passed(oneLine, hide=False)
+def exactHello():
+	"""prints \"Hello, world!\""""
+	assert "Hello, world!\n" == outputOf()
