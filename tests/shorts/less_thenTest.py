bfcd0875c2953da456a053c373d859bde5b58be6
AmirSahrani <115920196+AmirSahrani@users.noreply.github.com>
2025-01-06 16:09:41 +0100

    grammar

--- tests/shorts/less_thenTest.py
+++ tests/shorts/less_thenTest.py
@@ -7,16 +7,16 @@ only("less_then.py")
 def test1():
 	"""Checking input 5.0"""
 	output = outputOfExactStdin([5.0])
-	assert "5.0 is smaller then 10.0!" == output.strip()
+	assert "5.0 is smaller than 10.0!" == output.strip()
 
 @test()
 def test2():
 	"""Checking input 15.5"""
 	output = outputOfExactStdin([15.5])
-	assert "15.5 is not smaller then 10.0!" == output.strip()
+	assert "15.5 is not smaller than 10.0!" == output.strip()
 
 @test()
 def test3():
 	"""Checking input 10.0"""
 	output = outputOfExactStdin([10.0])
-	assert "10.0 is not smaller then 10.0!" == output.strip()
+	assert "10.0 is not smaller than 10.0!" == output.strip()
