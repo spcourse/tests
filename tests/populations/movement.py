42f8e2fccb5ff6804206509bfd58b2b6f2f1f757
unknown <wouter-vrielink@hotmail.com>
2025-03-31 16:01:35 +0200

    small correction and clarification

--- tests/populations/movement.py
+++ tests/populations/movement.py
@@ -29,12 +29,14 @@ def rabbit_step2():
 @passed(rabbit_values, hide=False)
 def rabbit_step3():
     """Rabbit turns around correctly at the edge
-        rabbit = Rabbit(0.95, 0.5, 0)
-        rabbit.step()"""
+        rabbit = Rabbit(0.995, 0.5, 0)
+        rabbit.step()
+
+    The rabbit should not move, and it's angle should become pi."""
 
     Rabbit = getModule().Rabbit._function
     rabbit = Rabbit(0.995, 0.5, 0)
     rabbit.step()
-    assert rabbit.pos_x == approx(0.995, abs=0.0001), "expected pos_x remains: 0.95"
+    assert rabbit.pos_x == approx(0.995, abs=0.0001), "expected pos_x remains: 0.995"
     assert rabbit.pos_y == approx(0.5, abs=0.0001), "expected pos_y remains: 0.5"
     assert rabbit.angle == approx(pi, abs=0.0001), "expected angle changed to: pi"
